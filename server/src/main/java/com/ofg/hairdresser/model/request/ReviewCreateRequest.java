207fbc4e5790c6ed718a1969f6cd43bb0ff3873e
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-11-06 01:38:58 +0300

    hairdresser id added in review create request object

--- server/src/main/java/com/ofg/hairdresser/model/request/ReviewCreateRequest.java
+++ server/src/main/java/com/ofg/hairdresser/model/request/ReviewCreateRequest.java
@@ -1,13 +1,16 @@
 package com.ofg.hairdresser.model.request;
 
+import com.ofg.hairdresser.model.entity.Hairdresser;
 import com.ofg.hairdresser.model.entity.Review;
 
 public record ReviewCreateRequest(
+        long hairdresserId,
         int rating,
         String comment
 ) {
-    public Review toReview() {
+    public Review toReview(Hairdresser hairdresser) {
         Review review = new Review();
+        review.setHairdresser(hairdresser);
         review.setRating(rating);
         review.setComment(comment);
         return review;
