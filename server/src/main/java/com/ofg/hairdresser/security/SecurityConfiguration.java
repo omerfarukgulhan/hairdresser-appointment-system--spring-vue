4ead65367d38875a77e84358f3ad75fb52936cc2
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-11-06 22:20:02 +0300

    routes secured and api response messages added

--- server/src/main/java/com/ofg/hairdresser/security/SecurityConfiguration.java
+++ server/src/main/java/com/ofg/hairdresser/security/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -29,11 +28,30 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests((authentication) ->
                         authentication
-                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.PUT, "/users/{id}")).authenticated()
-                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.PUT, "/users/update-password")).authenticated()
-                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.DELETE, "/users/{id}")).authenticated()
-                                .requestMatchers(HttpMethod.GET, "/users/test").hasAuthority("ROLE_ADMIN")
-                                .requestMatchers("/roles/**").hasAuthority("ROLE_ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/admin/{hairdresserId}").hasAuthority("ROLE_ADMIN")
+
+                                .requestMatchers(HttpMethod.GET, "/appointments").hasAuthority("ROLE_USER")
+                                .requestMatchers(HttpMethod.POST, "/appointments").hasAuthority("ROLE_USER")
+                                .requestMatchers(HttpMethod.PUT, "/appointments/{appointmentId}").hasAuthority("ROLE_USER")
+                                .requestMatchers(HttpMethod.DELETE, "/appointments/{appointmentId}").hasAuthority("ROLE_USER")
+
+                                .requestMatchers(HttpMethod.GET, "/hairdressers").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/hairdressers/{hairdresserId}").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/hairdressers").authenticated()
+                                .requestMatchers(HttpMethod.PUT, "/hairdressers/{hairdresserId}").hasAuthority("ROLE_HAIRDRESSER")
+                                .requestMatchers(HttpMethod.DELETE, "/hairdressers/{hairdresserId}").hasAuthority("ROLE_HAIRDRESSER")
+
+                                .requestMatchers(HttpMethod.GET, "/reviews").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/reviews").authenticated()
+                                .requestMatchers(HttpMethod.PUT, "/reviews/{reviewId}").authenticated()
+                                .requestMatchers(HttpMethod.DELETE, "/reviews/{reviewId}").authenticated()
+
+                                .requestMatchers(HttpMethod.GET, "/treatments/hairdresser/{hairdresserId}").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/treatments/{hairdresserId}").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/treatments").hasAuthority("ROLE_HAIRDRESSER")
+                                .requestMatchers(HttpMethod.PUT, "/treatments/{hairdresserId}").hasAuthority("ROLE_HAIRDRESSER")
+                                .requestMatchers(HttpMethod.DELETE, "/treatments/{hairdresserId}").hasAuthority("ROLE_HAIRDRESSER")
+
                                 .anyRequest().permitAll()
                 )
                 .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(authEntryPoint))
