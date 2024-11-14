22074b00ab0334320c9e9248f5481f8621c90a29
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-11-14 18:25:03 +0300

    router setup in main app, .env added in gitignore, store and router added in main.js

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,15 @@
-<script setup>
+<script>
+import {defineComponent} from "vue";
+import TheHeader from "@/components/layout/TheHeader.vue";
+
+export default defineComponent({
+  components: {TheHeader}
+})
 </script>
 
 <template>
-
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
