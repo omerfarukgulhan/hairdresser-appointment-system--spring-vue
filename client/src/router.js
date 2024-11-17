b827df47f52f6b3ef88ead92f6a7aa984cb726a6
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-11-18 00:32:22 +0300

    router updated for hairdresser dashboards

--- client/src/router.js
+++ client/src/router.js
@@ -7,37 +7,51 @@ import NotFound from "@/pages/NotFound.vue";
 import HairdresserDetailPage from "@/pages/hairdressers/HairdresserDetailPage.vue";
 import UserProfilePage from "@/pages/user/UserProfilePage.vue";
 import UserActivationPage from "@/pages/auth/UserActivationPage.vue";
+import HairdresserDashboardPage from "@/pages/hairdresser-management/HairdresserDashboardPage.vue";
+import HairdresserTreatmentsPage from "@/pages/hairdresser-management/HairdresserTreatmentsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', redirect: "/hairdressers"},
-    {path: '/hairdressers', name: "Hairdressers", component: HairdressersMainPage},
-    {path: '/hairdressers/:id', name: "HairdresserDetail", component: HairdresserDetailPage, props: true},
-    {path: '/profile', name: "UserProfile", component: UserProfilePage, meta: {requiresAuth: true}},
-    {path: '/login', name: "Login", component: LoginPage, meta: {requiresAuth: false}},
-    {path: '/register', name: "Register", component: RegisterPage, meta: {requiresAuth: false}},
+    {path: "/", redirect: "/hairdressers"},
+    {path: "/hairdressers", name: "Hairdressers", component: HairdressersMainPage},
+    {path: "/hairdressers/:id", name: "HairdresserDetail", component: HairdresserDetailPage, props: true},
+    {path: "/profile", name: "UserProfile", component: UserProfilePage, meta: {requiresAuth: true}},
+    {path: "/login", name: "Login", component: LoginPage, meta: {requiresAuth: false}},
+    {path: "/register", name: "Register", component: RegisterPage, meta: {requiresAuth: false}},
     {
-      path: '/users/activate/:activationToken',
-      name: 'UserActivation',
+      path: "/users/activate/:activationToken",
+      name: "UserActivation",
       component: UserActivationPage,
       meta: {requiresAuth: false}
     },
-    {path: '/:notFound(.*)', name: "NotFound", component: NotFound},
+    {
+      path: "/hairdresser/dashboard",
+      name: "HairdresserDashboard",
+      component: HairdresserDashboardPage,
+      meta: {requiresAuth: true, requiresHairdresser: true},
+    },
+    {
+      path: "/hairdresser/treatments",
+      name: "ManageTreatments",
+      component: HairdresserTreatmentsPage,
+      meta: {requiresAuth: true, requiresHairdresser: true},
+    },
+    {path: "/:notFound(.*)", name: "NotFound", component: NotFound},
   ],
 });
 
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = store.getters['auth/isAuthenticated'];
-  const isAdmin = store.getters['auth/isAdmin'];
-  const isHairdresser = store.getters['auth/isHairdresser'];
+  const isAuthenticated = store.getters["auth/isAuthenticated"];
+  const isAdmin = store.getters["auth/isAdmin"];
+  const isHairdresser = store.getters["auth/isHairdresser"];
 
   if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/login');
+    next("/login");
   } else if (to.meta.requiresAdmin && !isAdmin) {
-    next('/');
+    next("/");
   } else if (to.meta.requiresHairdresser && !isHairdresser) {
-    next('/login')
+    next("/login");
   } else {
     next();
   }
